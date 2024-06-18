059f39a660ed6ca7f8b0b084a805603f9aafce97
taylorwood95 <115211674+taylorwood95@users.noreply.github.com>
2024-06-18 15:02:28 +0100

    adding code

---
+++ src/main/java/com/jwt_demo/config/RsaKeyProperties.java
@@ -0,0 +1,12 @@
+package com.jwt_demo.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@ConfigurationProperties(prefix = "rsa")
+public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+
+}
